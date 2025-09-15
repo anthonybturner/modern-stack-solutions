00f0ab61e96ae7148507f498a3041c66ef615bc5
Anthony <anthonybturner@icloud.com>
2025-09-15 14:19:47 -0400

    spacing 8

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: 'home.html',
   styleUrl: 'home.scss',
 })
-export class Home { }
+export class Home {
+  constructor(private router:Router){}
+
+  GotoContact(): void{
+    this.router.navigate(['/contact']);
+  }
+}
